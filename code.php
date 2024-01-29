a395be2d8a664dfcd59867fa7ab28edc9eb4245b
takashi.sugou <takashi.sugou@betatechnology.jp>
2024-01-29 12:05:52 +0900

    a

---
+++ code.php
@@ -0,0 +1,34 @@
+<?php
+   $file = $_GET['file'];
+   if(isset($file))
+   {
+       include("pages/$file");
+   }
+   else
+   {
+       include("index.php");
+   }
+
+   $file = $_GET['file'];
+    include($file);
+
+    $file = $_GET['file'];
+    file_get_contents('directory/' . $file);
+
+    $con = mysql_connect("localhost","sqli","sqli");
+      if (!$con) {
+        die('Could not connect: ' . mysql_error());
+      }
+      mysql_select_db("sqliexample", $con);
+      $id = $_GET['id'];
+      $result = mysql_query("SELECT name FROM user WHERE id=$id", $con);
+    
+      mysql_close($con);
+      $num = mysql_num_rows($result);
+      $i=0;
+      while ($i < $num) {
+        $name = mysql_result($result, $i, "name");
+        echo "Hello " . $name;
+        echo "<br/>";
+        $i++;
+      }
